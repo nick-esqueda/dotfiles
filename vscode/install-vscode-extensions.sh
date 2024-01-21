d2612ecc2331b23864d6a1672e13315536914d70
nick-esqueda <nick_esqueda@outlook.com>
2024-01-21 11:03:42 -0600

    created vscode extension install script

---
+++ vscode/install-vscode-extensions.sh
@@ -0,0 +1,13 @@
+read -p "Install VSCode extensions? (Y|n): " response
+
+if [[ $response =~ ^[Nn](o)?$ ]] then
+	echo "aborting installation."
+	exit
+fi
+
+echo "installing extensions..."
+while read -r line; do
+	code --install-extension "$line"
+done < extensions.txt
+
+echo "done installing VSCode extensions."
