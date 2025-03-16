f95ee301af0e589e011255eeacd526a8bbeb21eb
nickesqueda <nick_esqueda@outlook.com>
2025-03-16 13:57:49 -0500

    neovim: updated obsidian.nvim config

--- nvim/lua/plugins/obsidian-nvim.lua
+++ nvim/lua/plugins/obsidian-nvim.lua
@@ -2,6 +2,9 @@
 
 vim.opt.conceallevel = 1 -- allow obsidian.nvim to conceal text (for links, etc.)
 
+vim.keymap.set("n", "<Leader>ob", ":ObsidianBacklinks<CR>")
+vim.keymap.set("n", "<Leader>os", ":ObsidianSearch<CR>")
+
 return {
   "epwalsh/obsidian.nvim",
   version = "*",
@@ -11,19 +14,48 @@ return {
     "nvim-lua/plenary.nvim",
   },
   opts = {
+    -- A list of workspace names, paths, and configuration overrides.
+    -- If you use the Obsidian app, the 'path' of a workspace should generally be
+    -- your vault root (where the `.obsidian` folder is located).
+    -- When obsidian.nvim is loaded by your plugin manager, it will automatically set
+    -- the workspace to the first workspace in the list whose `path` is a parent of the
+    -- current markdown file being edited.
     workspaces = {
       {
         name = "personal",
         path = "~/notes",
       },
     },
-  },
-  -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
-  completion = {
-    -- Set to false to disable completion.
-    nvim_cmp = true,
-    -- Trigger completion at 2 chars.
-    min_chars = 2,
+
+    -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+    completion = {
+      -- Set to false to disable completion.
+      nvim_cmp = true,
+      -- Trigger completion at 2 chars.
+      min_chars = 2,
+    },
+
+    -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
+    -- way then set 'mappings = {}'.
+    mappings = {
+      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+      ["gf"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+    },
+
+    -- Optional, boolean or a function that takes a filename and returns a boolean.
+    -- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
+    disable_frontmatter = true,
+
+    templates = {
+      folder = "~/notes/templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+    },
   },
 }
 
